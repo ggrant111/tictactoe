dd24c61816524cbc10e407c0fe0864d77e64baaa
ggrant111 <57473462+ggrant111@users.noreply.github.com>
2023-12-16 01:45:38 -0700

    updated win message and styles

--- game.js
+++ game.js
@@ -33,14 +33,14 @@ function isMainCellFull(mainCellIndex) {
 function checkSubBoardWinner(mainCellIndex) {
   const subBoard = gameState[mainCellIndex];
   const winConditions = [
-    [0, 1, 2],
+    [0, 1, 2], // Rows
     [3, 4, 5],
-    [6, 7, 8], // Rows
-    [0, 3, 6],
+    [6, 7, 8],
+    [0, 3, 6], // Columns
     [1, 4, 7],
-    [2, 5, 8], // Columns
-    [0, 4, 8],
-    [2, 4, 6], // Diagonals
+    [2, 5, 8],
+    [0, 4, 8], // Diagonals
+    [2, 4, 6],
   ];
 
   for (let condition of winConditions) {
@@ -50,11 +50,16 @@ function checkSubBoardWinner(mainCellIndex) {
       subBoard[a] === subBoard[b] &&
       subBoard[a] === subBoard[c]
     ) {
-      mainGameState[mainCellIndex] = currentPlayer;
-      return true;
+      mainGameState[mainCellIndex] = currentPlayer; // Update main game state
+
+      // Add class to the main cell to reflect the win
+      mainCells[mainCellIndex].classList.add(currentPlayer.toLowerCase());
+
+      return true; // Return true indicating a win
     }
   }
-  return false;
+
+  return false; // Return false if no win condition is met
 }
 
 function checkMainBoardWinner() {
@@ -86,6 +91,16 @@ function updateStatusText() {
   const statusTextElement = document.getElementById("statusText");
   statusTextElement.textContent = currentPlayer + "'s turn";
 }
+function checkForWinner() {
+  const winner = checkMainBoardWinner(); // Your existing function to check the winner
+  if (winner) {
+    document.getElementById("winnerMessage").textContent =
+      winner + " wins the Game!";
+    document.getElementById("winnerModal").style.display = "block";
+  }
+}
+
+// Call this function after each move to check for a winner
 
 function handleSubCellClick(event) {
   const subCell = event.target;
@@ -110,19 +125,26 @@ function handleSubCellClick(event) {
   subCell.textContent = currentPlayer; // Update with X and O styles
 
   if (checkSubBoardWinner(mainCellIndex)) {
-    mainCells[mainCellIndex].classList.add(currentPlayer.toLowerCase());
     const winner = checkMainBoardWinner();
     if (winner) {
       console.log(winner + " wins the main board!");
       const statusTextElement = document.getElementById("statusText");
       statusTextElement.textContent = winner + " wins the Game!";
-      // Handle the main board win (display message, end game, etc.)
+      checkForWinner();
+    } else {
+      // Continue the game if no main board winner yet
+      lastPlayedCell = subCellIndex;
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      updateStatusText();
+      checkForWinner();
     }
+  } else {
+    // Continue the game if no sub-board winner
+    lastPlayedCell = subCellIndex;
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    updateStatusText();
+    checkForWinner();
   }
-
-  lastPlayedCell = subCellIndex; // Update last played cell for game rules
-  currentPlayer = currentPlayer === "X" ? "O" : "X"; // Toggle player
-  updateStatusText();
 }
 
 function resetGame() {
@@ -153,3 +175,8 @@ subCells.forEach((subCell) => {
 });
 
 document.getElementById("resetButton").addEventListener("click", resetGame);
+document.getElementById("startNewGame").addEventListener("click", function () {
+  resetGame(); // Your existing reset game function
+  document.getElementById("winnerModal").style.display = "none";
+  updateStatusText();
+});
